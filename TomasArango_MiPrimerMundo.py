75b758d535a3cddb6dd6f37055dc49a635178dce
tarangon5225 <53449941+tarangon5225@users.noreply.github.com>
2019-07-29 16:53:30 -0500

    Add files via upload

---
+++ TomasArango_MiPrimerMundo.py
@@ -0,0 +1,23 @@
+'''Descripción: El objetivo es moverse hasta la casilla para
+recoger la manzana y dejar un banano y regresar
+a la casa.
+Hecho por: Tomás Arango Niño
+Fecha: 29/07/2019'''
+print("Hola soy Reborg")
+print("voy a hacer una misión")
+repeat 4:
+    move()
+#pause()
+take()
+#pause()
+put("banana")
+#pause()
+repeat 2:
+    turn_left()
+#pause()
+repeat 4:
+    move()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
